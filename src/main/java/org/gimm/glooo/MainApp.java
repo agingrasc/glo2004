7a34a9625affec2fe9c95751addd99d33a088261
JEISA5 <jerome.isabelle.2@ulaval.ca>
2016-10-10 18:40:16 -0400

    Unbelievable interface!
    -Ajout feuille css
    -Amelioration globale de l'interface

--- src/main/java/org/gimm/glooo/MainApp.java
+++ src/main/java/org/gimm/glooo/MainApp.java
@@ -2,6 +2,8 @@ package org.gimm.glooo;
 
 import java.io.IOException;
 
+import org.gimm.glooo.view.CreationGUIController;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
@@ -11,71 +13,76 @@ import javafx.scene.layout.BorderPane;
 public class MainApp extends Application
 {
 
-    private Stage primaryStage;
-    private BorderPane rootLayout;
+	private Stage primaryStage;
+	private BorderPane rootLayout;
 
-    @Override
-    public void start(Stage primaryStage)
-    {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("VisuaLigue");
+	@Override
+	public void start(Stage primaryStage)
+	{
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("VisuaLigue");
 
-        initRootLayout();
+		initRootLayout();
 
-        showCreationGUI();
+		showCreationGUI();
 
-    }
+	}
 
-    /**
-     * Initializes the root layout.
-     */
-    public void initRootLayout()
-    {
-        try
-        {
-            // Load root layout from fxml file.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("/fxml/RootLayout.fxml"));
-            rootLayout = (BorderPane) loader.load();
+	/**
+	 * Initializes the root layout.
+	 */
+	public void initRootLayout()
+	{
+		try
+		{
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/fxml/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
 
-            // Show the scene containing the root layout.
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
-    }
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Shows the CreationGUI inside the root layout.
-     */
-    public void showCreationGUI() 
-    {
- 
-        try 
-        {
-            // Load person overview.
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("/fxml/CreationGUI.fxml"));
-            BorderPane creationGUI = (BorderPane) loader.load();
+	/**
+	 * Shows the CreationGUI inside the root layout.
+	 */
+	public void showCreationGUI()
+	{
 
-            // Set person overview into the center of root layout.
-            rootLayout.setCenter(creationGUI);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		try
+		{
+			// Load CreationGui.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("/fxml/CreationGUI.fxml"));
+			BorderPane creationGUI = (BorderPane) loader.load();
 
+			// Set CreationGUI into the center of root layout.
+			rootLayout.setCenter(creationGUI);
 
-    public Stage getPrimaryStage()
-    {
-        return primaryStage;
-    }
+			// Give the controller access to the main app.
+			CreationGUIController controller = loader.getController();
+			controller.setMainApp(this);
 
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public Stage getPrimaryStage()
+	{
+		return primaryStage;
+	}
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
 }
---
+++ src/main/java/org/gimm/glooo/view/CreationGUIController.java
@@ -0,0 +1,65 @@
+package org.gimm.glooo.view;
+
+import org.gimm.glooo.MainApp;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class CreationGUIController
+{
+
+	// Reference to the main application.
+	private MainApp mainApp;
+
+	@FXML
+	void mouseClickedDelete(MouseEvent event)
+	{
+		System.out.println("mouseClickedDelete");
+	}
+
+	@FXML
+	void mouseClickedNext(MouseEvent event)
+	{
+		System.out.println("mouseClickedNext");
+	}
+
+	@FXML
+	void mouseClickedPrevious(MouseEvent event)
+	{
+		System.out.println("mouseClickedPrevious");
+	}
+
+	@FXML
+	void mouseClickedRedo(MouseEvent event)
+	{
+		System.out.println("mouseClickedRedo");
+	}
+
+	@FXML
+	void mouseClickedUndo(MouseEvent event)
+	{
+		System.out.println("mouseClickedUndo");
+	}
+
+	@FXML
+	void mouseClickedZoomIn(MouseEvent event)
+	{
+		System.out.println("mouseClickedZoomIn");
+	}
+
+	@FXML
+	void mouseClickedZoomOut(MouseEvent event)
+	{
+		System.out.println("mouseClickedZoomOut");
+	}
+
+	/**
+	 * Is called by the main application to give a reference back to itself.
+	 * 
+	 * @param mainApp
+	 */
+	public void setMainApp(MainApp mainApp)
+	{
+		this.mainApp = mainApp;
+	}
+}
