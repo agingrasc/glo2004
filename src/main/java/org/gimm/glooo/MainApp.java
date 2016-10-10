ec226aa0b487b32169d5d5c3231cd503dbe7f12f
JEISA5 <jerome.isabelle.2@ulaval.ca>
2016-10-10 03:42:19 -0400

    Suppression des sous-dossiers dans resources

--- src/main/java/org/gimm/glooo/MainApp.java
+++ src/main/java/org/gimm/glooo/MainApp.java
@@ -35,7 +35,7 @@ public class MainApp extends Application
         {
             // Load root layout from fxml file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("/org/gimm/glooo/view/RootLayout.fxml"));
+            loader.setLocation(MainApp.class.getResource("/fxml/RootLayout.fxml"));
             rootLayout = (BorderPane) loader.load();
 
             // Show the scene containing the root layout.
