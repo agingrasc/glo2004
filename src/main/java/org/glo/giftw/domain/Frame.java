e000620a5f7b6d1e703367ab06420fecbf2ea5db
algic1 <alexandre.gingras-courchesne.1@ulaval.ca>
2016-11-03 17:21:20 -0400

    unit test Frame

--- src/main/java/org/glo/giftw/domain/Frame.java
+++ src/main/java/org/glo/giftw/domain/Frame.java
@@ -1,16 +1,13 @@
 package org.glo.giftw.domain;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ArrayList;
 import java.io.Serializable;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class Frame implements Serializable
 {
     public static final long serialVersionUID = 1L;
     
-    private HashMap<Integer, GameObject> gameObjects;
+    private final HashMap<Integer, GameObject> gameObjects;
 
     public Frame()
     {
@@ -50,11 +47,20 @@ public class Frame implements Serializable
      * @param gameObject : The GameObject generating the collisions. 
      * @return An ArrayList of Integers containing the id of the colliding GameObjects (can be empty).
      */
-    public ArrayList<Integer> detectCollisions(GameObject gameObject)
+    public Set<Integer> detectCollisions(GameObject gameObject)
     {
-        return gameObjects.entrySet().stream().filter(
-                entry -> entry.getKey() != gameObject.id && gameObject.detectCollision(
-                        entry.getValue())).map(Map.Entry::getKey).collect(Collectors.toCollection(ArrayList::new));
+        Set<Integer> idInCollision = new HashSet<>();
+        Integer gameObjId = gameObject.getId();
+        for (GameObject gameObjToCheck : this.gameObjects.values())
+        {
+            Integer gameObjToCheckId = gameObjToCheck.getId();
+            if (!gameObjToCheckId.equals(gameObjId) && gameObjToCheck.detectCollision(gameObject))
+            {
+                idInCollision.add(gameObjToCheckId);
+            }
+        }
+
+        return idInCollision;
     }
     
     /**
@@ -62,9 +68,9 @@ public class Frame implements Serializable
      * @return A HashMap mapping the id of every GameObjects of the frame with an ArrayList of Integers containing the
      * id of the colliding GameObjects. Those ArrayLists may be empty.
      */
-    public HashMap<Integer, ArrayList<Integer>> detectCollisions()
+    public HashMap<Integer, Set<Integer>> detectCollisions()
     {
-        HashMap<Integer, ArrayList<Integer>> collisions = new HashMap<>();
+        HashMap<Integer, Set<Integer>> collisions = new HashMap<>();
         for(Map.Entry<Integer, GameObject> entry : gameObjects.entrySet())
         {
             collisions.put(entry.getKey(), this.detectCollisions(entry.getValue()));
