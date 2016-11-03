c5a299d6e27e76975eb8933e4b03479cc53670e7
almer59 <alexandra.mercier.4@ulaval.ca>
2016-11-03 17:10:15 -0400

    on peut maintenant s/lectionner une image pour l'obstacle

--- src/main/java/org/glo/giftw/view/NewObstacleController.java
+++ src/main/java/org/glo/giftw/view/NewObstacleController.java
@@ -2,9 +2,21 @@ package org.glo.giftw.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.DialogPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Window;
+import java.io.File;
 
 public class NewObstacleController
 {
+	@FXML
+	private ImageView imagePreview;
+
+	@FXML
+	private DialogPane newObstacleDialog;
+
 	@FXML
 	void onActionModify(ActionEvent event)
 	{
@@ -14,6 +26,10 @@ public class NewObstacleController
 	@FXML
 	void onActionOpen(ActionEvent event)
 	{
-		System.out.println("onActionOpen");
+		Window parentWindow = newObstacleDialog.getScene().getWindow();
+		OpenImageFileController openImageFileController = new OpenImageFileController();
+
+		File imageToOpen = openImageFileController.startDialog(parentWindow);
+		imagePreview.setImage(new Image(imageToOpen.toURI().toString()));
 	}
 }
--- src/main/java/org/glo/giftw/view/NewSportController.java
+++ src/main/java/org/glo/giftw/view/NewSportController.java
@@ -3,12 +3,17 @@ package org.glo.giftw.view;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import java.io.File;
+
 public class NewSportController
 {
 	@FXML
 	void onActionDelete(ActionEvent event)
 	{
 		System.out.println("onActionDelete");
+		OpenImageFileController openImageFileController = new OpenImageFileController();
+
+		File imageToOpen = openImageFileController.startDialog();
 	}
 
 	@FXML
