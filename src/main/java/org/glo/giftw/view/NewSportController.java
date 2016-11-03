59a0f71f39f20d1e0fbd75e9d0d89ed5452a603a
almer59 <alexandra.mercier.4@ulaval.ca>
2016-11-03 17:15:52 -0400

    new Sport a maintenant la structure pour loader des images

--- src/main/java/org/glo/giftw/view/NewSportController.java
+++ src/main/java/org/glo/giftw/view/NewSportController.java
@@ -2,18 +2,24 @@ package org.glo.giftw.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DialogPane;
+import javafx.stage.Window;
 
 import java.io.File;
 
 public class NewSportController
 {
 	@FXML
+	private DialogPane newSportDialog;
+
+	@FXML
 	void onActionDelete(ActionEvent event)
 	{
+		Window parentWindow = newSportDialog.getScene().getWindow();
 		System.out.println("onActionDelete");
 		OpenImageFileController openImageFileController = new OpenImageFileController();
 
-		File imageToOpen = openImageFileController.startDialog();
+		File imageToOpen = openImageFileController.startDialog(parentWindow);
 	}
 
 	@FXML
