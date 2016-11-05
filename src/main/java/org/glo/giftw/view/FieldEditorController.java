056d8ba624c9997cfc15a26263fc16136c6c47fa
almer59 <alexandra.mercier.4@ulaval.ca>
2016-11-05 13:16:56 -0400

    le fond du terrain peut être changé

--- src/main/java/org/glo/giftw/view/FieldEditorController.java
+++ src/main/java/org/glo/giftw/view/FieldEditorController.java
@@ -4,14 +4,13 @@ package org.glo.giftw.view;
  * Created by alexandra on 11/4/16.
  */
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DialogPane;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
@@ -37,29 +36,72 @@ public class FieldEditorController
     private ChoiceBox fieldSize;
 
     @FXML
+    private ColorPicker fieldColor;
+
+    @FXML
     public void initialize() {
         System.out.println("initializeFieldEditor");
 
         initCanvas();
-
+        initSpinners();
         initChoiceBox();
     }
 
-    private void initCanvas()
+    private void initSpinners()
     {
-        GraphicsContext gc = fieldDraw.getGraphicsContext2D();
-        gc.setFill(Color.BLUE);
-
         double fieldInitialLength = (fieldDraw.getWidth()) / 100;
         double fieldInitialWidth = (fieldDraw.getHeight()) / 100;
 
         fieldLength.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 100, fieldInitialLength));
         fieldWidth.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 100, fieldInitialWidth));
+
+        fieldLength.setEditable(true);
+        fieldWidth.setEditable(true);
+    }
+
+    private void initCanvas()
+    {
+        GraphicsContext gc = fieldDraw.getGraphicsContext2D();
+        gc.setFill(Color.WHITE);
+        gc.fillRect(0, 0, fieldDraw.getWidth(), fieldDraw.getHeight());
     }
 
     private void initChoiceBox()
     {
         fieldSize.getItems().addAll("m²", "pi²");
         fieldSize.setValue("m²");
+
+        fieldSize.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                System.out.println("selection changed!");
+                if(fieldSize.getSelectionModel().isSelected(1)) //pi carré
+                {
+                    fieldLength.getValueFactory().setValue((fieldDraw.getWidth()/100)*3.28);
+                    fieldWidth.getValueFactory().setValue((fieldDraw.getHeight()/100)*3.28);
+                }
+                else if(fieldSize.getSelectionModel().isSelected(0)) // mètre carré
+                {
+                    fieldLength.getValueFactory().setValue((fieldDraw.getWidth()/100));
+                    fieldWidth.getValueFactory().setValue((fieldDraw.getHeight()/100));
+                }
+            }
+        });
     }
+
+    @FXML
+    public void onSave()
+    {
+        System.out.println("onSave");
+    }
+
+    @FXML
+    public void onFieldColorChanged()
+    {
+        System.out.println("onColorChanged");
+        GraphicsContext gc = fieldDraw.getGraphicsContext2D();
+        gc.setFill(fieldColor.getValue());
+        gc.fillRect(0, 0, fieldDraw.getWidth(), fieldDraw.getHeight());
+    }
+
 }
