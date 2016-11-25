b45b8789faaee9b4565b46e5a29ae278baf7b4ec
algic1 <alexandre.gingras-courchesne.1@ulaval.ca>
2016-11-25 11:13:02 -0500

    fix indent stupide et isolation probleme: roles

--- src/main/java/org/glo/giftw/view/NewSportController.java
+++ src/main/java/org/glo/giftw/view/NewSportController.java
@@ -17,145 +17,152 @@ import java.util.Optional;
 
 public class NewSportController
 {
-	@FXML
-	private DialogPane rootDialogPane;
+    @FXML
+    private DialogPane rootDialogPane;
 
-	@FXML
-	private TextField sportName;
+    @FXML
+    private TextField sportName;
 
-	@FXML
-	private ComboBox<String> roles;
+    @FXML
+    private ComboBox<String> roles;
 
-	@FXML
-	private TextField maxPlayers;
+    @FXML
+    private TextField maxPlayers;
 
-	@FXML
-	private TextField maxTeams;
+    @FXML
+    private TextField maxTeams;
 
-	@FXML
-	private TextField projectileName;
+    @FXML
+    private TextField projectileName;
 
-	@FXML
-	private ImageView fieldImage;
+    @FXML
+    private ImageView fieldImage;
 
-	@FXML
-	private ImageView fieldProjectile;
+    @FXML
+    private ImageView fieldProjectile;
 
-	@FXML
-	private Spinner<Double> fieldLength;
+    @FXML
+    private Spinner<Double> fieldLength;
 
-	@FXML
-	private Spinner<Double> fieldWidth;
+    @FXML
+    private Spinner<Double> fieldWidth;
 
-	public File getSportFieldImageFile() { return new File(fieldImage.getImage().toString()); }
+    public File getSportFieldImageFile()
+    {
+        return new File(fieldImage.getImage().toString());
+    }
 
-	public File getSportProjectileImageFile() { return new File(fieldProjectile.getImage().toString()); }
+    public File getSportProjectileImageFile()
+    {
+        return new File(fieldProjectile.getImage().toString());
+    }
 
-	@FXML
-	public void initialize() {
-		System.out.println("initializeNewSport");
-		initSpinners(0, 0);
-	}
+    @FXML
+    public void initialize()
+    {
+        System.out.println("initializeNewSport");
+        initSpinners(0, 0);
+    }
 
-	private void initSpinners(double initialLength, double initialWidth)
-	{
-		fieldLength.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 100, initialLength));
-		fieldWidth.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 100, initialWidth));
+    private void initSpinners(double initialLength, double initialWidth)
+    {
+        fieldLength.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 100, initialLength));
+        fieldWidth.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(0, 100, initialWidth));
 
-		fieldLength.setEditable(true);
-		fieldWidth.setEditable(true);
-	}
+        fieldLength.setEditable(true);
+        fieldWidth.setEditable(true);
+    }
 
-	@FXML
-	void onActionDraw(ActionEvent event) throws IOException
-	{
-		System.out.println("onActionDraw");
+    @FXML
+    void onActionDraw(ActionEvent event) throws IOException
+    {
+        System.out.println("onActionDraw");
 
-		Dialog<Object> dialog = new Dialog<Object>();
+        Dialog<Object> dialog = new Dialog<Object>();
 
-		FXMLLoader loader = new FXMLLoader();
+        FXMLLoader loader = new FXMLLoader();
 
-		loader.setLocation(getClass().getResource(FXMLPaths.FIELD_EDITOR_PATH.toString()));
-		DialogPane fieldEditorDialogPane = loader.load();
+        loader.setLocation(getClass().getResource(FXMLPaths.FIELD_EDITOR_PATH.toString()));
+        DialogPane fieldEditorDialogPane = loader.load();
 
-		dialog.setDialogPane(fieldEditorDialogPane);
-		FieldEditorController fieldEditorController = loader.<FieldEditorController>getController();
+        dialog.setDialogPane(fieldEditorDialogPane);
+        FieldEditorController fieldEditorController = loader.<FieldEditorController>getController();
 
-		fieldEditorController.initSpinners((double)fieldLength.getValue(), (double)fieldWidth.getValue());
+        fieldEditorController.initSpinners((double) fieldLength.getValue(), (double) fieldWidth.getValue());
 
-		dialog.showAndWait();
+        dialog.showAndWait();
 
-		setImage(fieldEditorController.getDrawnFieldFilePath(), fieldImage);
-		if(fieldEditorController.getDrawnFieldFilePath()!=null) {
-			double newLength = fieldEditorController.getLength();
-			double newWidth = fieldEditorController.getWidth();
-			if( newLength > 0 && newWidth > 0 ) { initSpinners(newLength, newWidth); }
-		}
+        setImage(fieldEditorController.getDrawnFieldFilePath(), fieldImage);
+        if (fieldEditorController.getDrawnFieldFilePath() != null)
+        {
+            double newLength = fieldEditorController.getLength();
+            double newWidth = fieldEditorController.getWidth();
+            if (newLength > 0 && newWidth > 0)
+            {
+                initSpinners(newLength, newWidth);
+            }
+        }
 
-	}
+    }
 
-	@FXML
-	void onActionBrowseField(ActionEvent event)
-	{
-		System.out.println("onActionBrowseField");
-		Window parentWindow = rootDialogPane.getScene().getWindow();
-		ImageFileController imageFileController = new ImageFileController();
+    @FXML
+    void onActionBrowseField(ActionEvent event)
+    {
+        System.out.println("onActionBrowseField");
+        Window parentWindow = rootDialogPane.getScene().getWindow();
+        ImageFileController imageFileController = new ImageFileController();
 
-		File imageToOpen = imageFileController.startOpenFileDialog(parentWindow);
-		setImage(imageToOpen, fieldImage);
-	}
+        File imageToOpen = imageFileController.startOpenFileDialog(parentWindow);
+        setImage(imageToOpen, fieldImage);
+    }
 
-	@FXML
-	void onActionBrowseProjectile(ActionEvent event)
-	{
-		System.out.println("onActionBrowseProjectile");
-		Window parentWindow = rootDialogPane.getScene().getWindow();
-		ImageFileController imageFileController = new ImageFileController();
+    @FXML
+    void onActionBrowseProjectile(ActionEvent event)
+    {
+        System.out.println("onActionBrowseProjectile");
+        Window parentWindow = rootDialogPane.getScene().getWindow();
+        ImageFileController imageFileController = new ImageFileController();
 
-		File imageToOpen = imageFileController.startOpenFileDialog(parentWindow);
-		setImage(imageToOpen, fieldProjectile);
-	}
+        File imageToOpen = imageFileController.startOpenFileDialog(parentWindow);
+        setImage(imageToOpen, fieldProjectile);
+    }
 
-	private void setImage(File imageToOpen, ImageView imageView)
-	{
-		if(imageToOpen != null)
-		{
-			/*if ( (double)fieldLength.getValue() > 0 && (double)fieldWidth.getValue() > 0) {
+    private void setImage(File imageToOpen, ImageView imageView)
+    {
+        if (imageToOpen != null)
+        {
+            /*if ( (double)fieldLength.getValue() > 0 && (double)fieldWidth.getValue() > 0) {
 				fieldImage.setFitHeight((double)fieldWidth.getValue() * 100);
 				fieldImage.setFitWidth((double)fieldLength.getValue() * 100);
 			}*/
-			imageView.setImage(new Image(imageToOpen.toURI().toString()));
-		}
-	}
+            imageView.setImage(new Image(imageToOpen.toURI().toString()));
+        }
+    }
 
-	public void showDialog() throws IOException
-	{
-		Dialog<ButtonType> dialog = new Dialog<ButtonType>();
-		dialog.setDialogPane(rootDialogPane);
-		Optional<ButtonType> result = dialog.showAndWait();
+    public void showDialog() throws IOException
+    {
+        Dialog<ButtonType> dialog = new Dialog<ButtonType>();
+        dialog.setDialogPane(rootDialogPane);
+        Optional<ButtonType> result = dialog.showAndWait();
 
-		if (result.isPresent() && result.get() == ButtonType.FINISH)
-		{
-			//FIXME: completer l'interface
+        if (result.isPresent() && result.get() == ButtonType.FINISH)
+        {
+            //FIXME: completer l'interface
             String name = sportName.getText();
-			List<String> lroles = Arrays.asList("un", "deux", "trois");
-			Integer flength = Integer.parseInt(fieldLength.getEditor().getText());
-			Integer fwidth = Integer.parseInt(fieldWidth.getEditor().getText());
-			//Integer flength = 5;
-			//Integer fwidth = 2;
+            List<String> lroles = Arrays.asList("un", "deux", "trois");
+            //List<String> lroles = roles.getItems();
+            Integer flength = Integer.parseInt(fieldLength.getEditor().getText());
+            Integer fwidth = Integer.parseInt(fieldWidth.getEditor().getText());
             String sportImgPath = getSportFieldImageFile().getPath();
-			//String projName = projectileName.getText();
-			String projName = "Puck";
-            //String projPath = getSportProjectileImageFile().getPath();
-			String projPath = "/home/alexandre/git/glo2004/test.png";
-            //Integer maxNumberOfPlayer = Integer.parseInt(maxPlayers.getText());
-			//Integer maxNumberOfTeams = Integer.parseInt(maxTeams.getText());
-			Integer maxNumberOfPlayer = 6;
-			Integer maxNumberOfTeams = 2;
+            String projName = projectileName.getText();
+            String projPath = getSportProjectileImageFile().getPath();
+            Integer maxNumberOfPlayer = Integer.parseInt(maxPlayers.getText());
+            Integer maxNumberOfTeams = Integer.parseInt(maxTeams.getText());
 
-			Controller.getInstance().createSport(name, lroles, flength, fwidth, sportImgPath, projName, projPath, maxNumberOfPlayer, maxNumberOfTeams);
+            Controller.getInstance().createSport(name, lroles, flength, fwidth, sportImgPath, projName, projPath,
+                                                 maxNumberOfPlayer, maxNumberOfTeams);
 
-			RootLayoutController.getInstance().getOpenSportController().updateTable();
-		}
-	}
+            RootLayoutController.getInstance().getOpenSportController().updateTable();
+        }
+    }
 }
