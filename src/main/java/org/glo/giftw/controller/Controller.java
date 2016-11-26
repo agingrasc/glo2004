d23fec749404d366a81ac223b210e16087965e88
algic1 <alexandre.gingras-courchesne.1@ulaval.ca>
2016-11-26 16:02:16 -0500

    fix bogue serializable pour sport

--- src/main/java/org/glo/giftw/controller/Controller.java
+++ src/main/java/org/glo/giftw/controller/Controller.java
@@ -13,7 +13,7 @@ public class Controller
     private StrategyPool strategyPool;
 
     private Strategy currentStrategy;
-    
+
     private static Controller INSTANCE = null;
 
     private Controller()
@@ -24,37 +24,39 @@ public class Controller
         this.strategyPool = new StrategyPool();
         this.currentStrategy = null;
     }
-    
+
     public static Controller getInstance()
     {
-        if(Controller.INSTANCE == null)
+        if (Controller.INSTANCE == null)
         {
             Controller.INSTANCE = new Controller();
         }
         return Controller.INSTANCE;
     }
-    
+
     /**
      * Crée un nouveau sport et le sauvegarde dans SportPool.
-     * @param name Le nom du sport.
-     * @param roles La liste des rôles associés au sport.
-     * @param fieldLength La largeur du terrain (composante x).
-     * @param fieldHeight La hauteur du terrain (composante y).
-     * @param projectileName Le nom du projectile du sport.
+     *
+     * @param name                Le nom du sport.
+     * @param roles               La liste des rôles associés au sport.
+     * @param fieldLength         La largeur du terrain (composante x).
+     * @param fieldHeight         La hauteur du terrain (composante y).
+     * @param projectileName      Le nom du projectile du sport.
      * @param projectileImagePath Le chemin vers l'image associé au projectile.
-     * @param maxPLayersPerTeam Le nombre maximum de joueurs par équipe sur le terrain à un moment donné.
-     * @param maxTeams Le nombre maximum d'équipe lors d'une partie.
+     * @param maxPLayersPerTeam   Le nombre maximum de joueurs par équipe sur le terrain à un moment donné.
+     * @param maxTeams            Le nombre maximum d'équipe lors d'une partie.
      */
-    public void createSport(String name, List<String> roles, int fieldLength, int fieldHeight, String fieldImagePath, 
-            String projectileName, String projectileImagePath, int maxPLayersPerTeam, int maxTeams)
+    public void createSport(String name, List<String> roles, int fieldLength, int fieldHeight, String fieldImagePath,
+                            String projectileName, String projectileImagePath, int maxPLayersPerTeam, int maxTeams)
     {
         this.sportPool.addSport(name, roles, new Vector(fieldLength, fieldHeight), fieldImagePath, projectileName,
-                projectileImagePath, maxPLayersPerTeam, maxTeams);
+                                projectileImagePath, maxPLayersPerTeam, maxTeams);
     }
-    
+
     /**
      * Crée un nouveau type d'obstacle et le sauvegarde dans Obstacle Pool.
-     * @param name Le nom de l'obstacle.
+     *
+     * @param name      Le nom de l'obstacle.
      * @param imagePath Le chemin vers l'image associé à l'obstacle.
      */
     public void createObstacle(String name, boolean isCollidable, String imagePath)
@@ -64,7 +66,8 @@ public class Controller
 
     /**
      * Crée une nouvelle stratégie.
-     * @param name Le nom de la nouvelle stratédie.
+     *
+     * @param name      Le nom de la nouvelle stratédie.
      * @param sportName Le nom du sport (déjà existant) associé à la stratégie.
      */
     public void createStrategy(String name, String sportName)
@@ -75,9 +78,10 @@ public class Controller
 
     /**
      * Crée un nouveau joueur dans la stratégie et l'ajoute dans la frame courante.
-     * @param position La position initiale du joueur.
+     *
+     * @param position    La position initiale du joueur.
      * @param orientation L'orientation initiale du joueur.
-     * @param dimensions Les dimensions initiales du joueur.
+     * @param dimensions  Les dimensions initiales du joueur.
      * @return L'id du joueur nouvellement créé.
      */
     public Integer addPlayer(Vector position, float orientation, Vector dimensions)
@@ -87,9 +91,10 @@ public class Controller
 
     /**
      * Crée un nouvel obstacle dans la stratégie et l'ajoute dans la frame courante.
-     * @param position La position initiale de l'obstacle.
+     *
+     * @param position    La position initiale de l'obstacle.
      * @param orientation L'orientation initiale de l'obstacle.
-     * @param dimensions Les dimensions initiales de l'obstacle.
+     * @param dimensions  Les dimensions initiales de l'obstacle.
      * @return L'id de l'obstacle nouvellement créé.
      */
     public Integer addObstacle(String name, Vector position, float orientation, Vector dimensions)
@@ -100,9 +105,10 @@ public class Controller
 
     /**
      * Crée un nouveau projectile dans la stratégie et l'ajoute dans la frame courante.
-     * @param position La position initiale du projectile.
+     *
+     * @param position    La position initiale du projectile.
      * @param orientation L'orientation initiale du projectile.
-     * @param dimensions Les dimensions initiales du projectile.
+     * @param dimensions  Les dimensions initiales du projectile.
      * @return L'id du projectile nouvellement créé.
      */
     public Integer addProjectile(Vector position, float orientation, Vector dimensions)
@@ -114,60 +120,65 @@ public class Controller
     {
         this.currentStrategy.placeGameObject(gameObject, position, orientation, dimensions);
     }
-    
+
     /**
      * Fourni une description des sports pour le menu de sélection d'un sport de la vue.
+     *
      * @return Une hashmap associant le nom des sports aux chemins des image de son terrain.
      */
     public HashMap<String, String> getSportDescription()
     {
         return this.sportPool.getSportsDescription();
     }
-    
+
     public Collection<Obstacle> getObstacles()
     {
-    	return this.obstaclePool.getAllObstacles();
+        return this.obstaclePool.getAllObstacles();
     }
-    
+
     public Collection<Sport> getSports()
     {
-    	return this.sportPool.getAllSports();
+        return this.sportPool.getAllSports();
     }
-    
+
     public Collection<Strategy> getStrategies()
     {
-    	return this.strategyPool.getAllStrategies();
+        return this.strategyPool.getAllStrategies();
     }
-    
+
     /**
      * Retourne la frame courante de la stratégie.
+     *
      * @return la frame courante.
      */
     public Frame getCurrentFrame()
     {
         return this.currentStrategy.getCurrentFrame();
     }
-    
+
     /**
-     * Retourne la frame précédente de la stratégie. 
+     * Retourne la frame précédente de la stratégie.
+     *
      * @return La frame précédente.
      */
     public Frame previousFrame()
     {
         return this.currentStrategy.previousFrame();
     }
-    
+
     /**
-     * Retourne la frame suivante de la stratégie. 
+     * Retourne la frame suivante de la stratégie.
+     *
      * @return La frame suivante.
      */
     public Frame nextFrame()
     {
         return this.currentStrategy.nextFrame();
     }
-    
+
     /**
      * Crée une nouvelle frame à la fin de la suite de frames.
+     *
      * @return La nouvelle frame.
      */
     public Frame createNewFrame()
@@ -176,10 +187,11 @@ public class Controller
         this.currentStrategy.createNewFrame();
         return this.currentStrategy.nextFrame();
     }
-    
+
     /**
      * Permet de modifier la frame courante de la stratégie selon un delta de temps, en secondes, précis auxs dixième de
      * secondes.
+     *
      * @param delta La longueur du saut entre les frames, en secondes.
      * @return La nouvelle frame courante.
      */
