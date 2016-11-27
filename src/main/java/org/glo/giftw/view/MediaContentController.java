59b6454fb6b65f428b3f9c809a2dc3cf3257a400
algic1 <alexandre.gingras-courchesne.1@ulaval.ca>
2016-11-27 17:10:25 -0500

    affichage terrain base ok

--- src/main/java/org/glo/giftw/view/MediaContentController.java
+++ src/main/java/org/glo/giftw/view/MediaContentController.java
@@ -1,12 +1,27 @@
 package org.glo.giftw.view;
 
 import javafx.fxml.FXML;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import org.glo.giftw.domain.Controller;
+
+import java.io.File;
 
 public class MediaContentController
 {
 	@FXML
 	private StackPane rootStackPane;
+
+	public void displayNewFrame()
+	{
+		rootStackPane.getChildren().clear();
+		Controller.getInstance().createNewFrame();
+		File file = new File(Controller.getInstance().getSportFieldImagePath());
+		Image sportFieldImage = new Image(file.toURI().toString());
+		BackgroundSize bgSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
+		BackgroundPosition bgPos = BackgroundPosition.CENTER;
+		rootStackPane.setBackground(new Background(new BackgroundImage(sportFieldImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, bgPos, bgSize)));
+	}
 
 	public StackPane getRootStackPane()
 	{
--- src/main/java/org/glo/giftw/view/ModeToolBarController.java
+++ src/main/java/org/glo/giftw/view/ModeToolBarController.java
@@ -1,14 +1,13 @@
 package org.glo.giftw.view;
 
-import java.io.IOException;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToolBar;
 import org.glo.giftw.domain.Controller;
 import org.glo.giftw.domain.TreeViewable;
 import org.glo.giftw.domain.exceptions.StrategyNotFound;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.ToolBar;
+import java.io.IOException;
 
 public class ModeToolBarController
 {
@@ -42,6 +41,19 @@ public class ModeToolBarController
 	@FXML
 	void onActionWatch(ActionEvent event) throws IOException 
 	{
+		TreeViewable strategy = RootLayoutController.getInstance().getOpenStrategyController().getTreeTableView().getSelectionModel().getSelectedItem().getValue();
+		if(strategy != null)
+		{
+			try
+			{
+				Controller.getInstance().openStrategy(strategy.getDisplayName());
+			}
+			catch (StrategyNotFound e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		RootLayoutController.getInstance().watch();
 	}
 
