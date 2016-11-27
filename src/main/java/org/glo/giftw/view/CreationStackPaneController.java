12c82a92d5567fd884c9efcb034f3fe138676999
JEISA5 <jerome.isabelle.2@ulaval.ca>
2016-11-27 13:59:19 -0500

    Affichage des items dans le menu a droite
    Bugs causés par domaine non a jour

--- src/main/java/org/glo/giftw/view/CreationStackPaneController.java
+++ src/main/java/org/glo/giftw/view/CreationStackPaneController.java
@@ -6,7 +6,6 @@ import org.glo.giftw.domain.Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.Group;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -35,38 +34,10 @@ public class CreationStackPaneController
 	
 	private Pane currentPane;
 	
+	private double zoomFactor = 1;
+	
 	@FXML
     private ScrollPane scrollPane;
-
-
-	@FXML
-	void onDragOver(DragEvent event)
-	{
-		if (event.getDragboard().hasImage())
-		{
-			event.acceptTransferModes(TransferMode.COPY);
-		}
-
-		event.consume();
-	}
-
-	@FXML
-	void onDragDropped(DragEvent event)
-	{
-		Dragboard db = event.getDragboard();
-		boolean success = false;
-		if (db.hasImage())
-		{
-			ImageView imageView = new ImageView(db.getImage());
-			imageView.setX(event.getX());
-			imageView.setY(event.getY());
-			currentPane.getChildren().add(imageView);
-			success = true;
-		}
-		event.setDropCompleted(success);
-
-		event.consume();
-	}
 	
 	public void displayNewFrame()
 	{
@@ -76,19 +47,56 @@ public class CreationStackPaneController
 		BackgroundSize bgSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
 		stackPane.setBackground(new Background(new BackgroundImage(sportFieldImage,BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT,null,bgSize)));
 		stackPane.setPrefSize(scrollPane.getWidth(),scrollPane.getHeight());
-		stackPane.getChildren().add(new Label("LABEL"));
+		createCurrentGroup();
+	}
+	
+	private void createCurrentGroup()
+	{
+		currentPane = new Pane();
+		currentPane.setBackground(Background.EMPTY);
+		stackPane.getChildren().add(currentPane);
+		currentPane.prefWidthProperty().bind(stackPane.widthProperty());
+		currentPane.prefHeightProperty().bind(stackPane.heightProperty());
+		
+		currentPane.setOnDragOver((DragEvent event) -> {
+			System.out.println("hey2");
+			if (event.getDragboard().hasImage())
+			{
+				event.acceptTransferModes(TransferMode.COPY);
+			}
+
+			event.consume();
+		});
+		
+		currentPane.setOnDragDropped((DragEvent event) -> {
+			Dragboard db = event.getDragboard();
+			boolean success = false;
+			if (db.hasImage())
+			{
+				ImageView imageView = new ImageView(db.getImage());
+				imageView.setX(event.getX());
+				imageView.setY(event.getY());
+				currentPane.getChildren().add(imageView);
+				success = true;
+			}
+			event.setDropCompleted(success);
+
+			event.consume();
+		});
 	}
 	
 	public void zoomIn()
 	{
 		Scale scaleTransform = new Scale(1.25, 1.25, 0, 0);
 		subGroup.getTransforms().add(scaleTransform);
+		zoomFactor *= 1.25;
 	}
 	
 	public void zoomOut()
 	{
 		Scale scaleTransform = new Scale(0.75, 0.75, 0, 0);
 		subGroup.getTransforms().add(scaleTransform);
+		zoomFactor *= 0.75;
 	}
 	
 	@FXML
