d2cbe0b395a28dfd17f75492080bf815f68bcf6c
almer59 <alexandra.mercier.4@ulaval.ca>
2016-12-17 18:58:06 -0500

    exportation du joueur(fleches non fonctionnels)

--- src/main/java/org/glo/giftw/view/StrategyImageExporter.java
+++ src/main/java/org/glo/giftw/view/StrategyImageExporter.java
@@ -29,14 +29,18 @@ public class StrategyImageExporter {
         System.out.println("BuildingStrategyExportation");
 
         controller = Controller.getInstance();
-        dimensions = controller.getFieldDimensions();
 
-        content = new Canvas(dimensions.getX(), dimensions.getY());
+        File fieldPath = new File(controller.getSportFieldImagePath());
+        Image fieldBackground = new Image(fieldPath.toURI().toString());
+
+        content = new Canvas(fieldBackground.getWidth(), fieldBackground.getHeight());
         gc = content.getGraphicsContext2D();
         gcDraw = new GraphicContextDrawController();
-        File fieldPath = new File(controller.getSportFieldImagePath());
 
-        gcDraw.drawImage(gc, new Image(fieldPath.toURI().toString()));
+        computeAndSetPixelToUnitRatio();
+        dimensions = controller.getFieldDimensions();
+
+        gcDraw.drawImage(gc, fieldBackground);
 
         drawObjects();
     }
@@ -55,6 +59,20 @@ public class StrategyImageExporter {
         }
     }
 
+    private void computeAndSetPixelToUnitRatio()
+    {
+        Vector fieldDimensions = Controller.getInstance().getFieldDimensions();
+        double adjustedWidth = gc.getCanvas().getWidth();
+        double adjustedHeight = gc.getCanvas().getHeight();
+        System.out.println(adjustedHeight);
+        System.out.println(adjustedWidth);
+        System.out.println(fieldDimensions.getY());
+        System.out.println(fieldDimensions.getX());
+        Vector ratioPixelToUnit = new Vector(adjustedWidth / fieldDimensions.getX(),
+                adjustedHeight / fieldDimensions.getY());
+        controller.setPixelToUnitRatio(ratioPixelToUnit);
+    }
+
     private void drawGameObject(GameObject gameObject, String filePath, Vector size) {
         Vector position = controller.getPosition(gameObject);
 
@@ -62,19 +80,26 @@ public class StrategyImageExporter {
         gcDraw.drawImage(gc, new Image(file.toURI().toString()), position.getX(), position.getY(), size.getX(), size.getY());
     }
 
+    private void centerPosition(Vector position, Vector size)
+    {
+        position.setX(position.getX() + (0.5*size.getX()));
+        position.setY(position.getY() + (0.5*size.getY()));
+    }
+
+
     private void tracePlayerMovement(GameObject player) {
         gcDraw.setStrokeLineSize(gc, 5);
         setAtFirstFrame();
+        Vector size = controller.getDimensions(player);
         Vector prevPosition = controller.getPosition(player);
+        centerPosition(prevPosition, size);
         Vector nextPosition;
-
-        Frame frame;
 
         while (!controller.isLastFrame()) {
             controller.nextFrame();
-            frame = controller.getCurrentFrame();
-            nextPosition = frame.getPosition(player);
-            if (frame.isKeyFrame()) {
+            nextPosition = controller.getPosition(player);
+            centerPosition(nextPosition, size);
+            if (controller.getCurrentFrame().isKeyFrame()) {
                 gcDraw.drawArrow(gc, prevPosition.getX(), prevPosition.getY(), nextPosition.getX(), nextPosition.getY());
             } else {
                 gcDraw.drawLine(gc, prevPosition.getX(), prevPosition.getY(), nextPosition.getX(), nextPosition.getY());
@@ -108,24 +133,27 @@ public class StrategyImageExporter {
         gcDraw.setStrokeLineStyle(gc, false);
     }
 
-    private void drawPlayer(GameObject player, Vector position) {
+    private void drawPlayer(GameObject player) {
+        Vector position = controller.getPosition(player);
+        Vector size = controller.getDimensions(player);
+
+        position.setY(position.getY()+(0.5*size.getY()));
+
         String team = controller.getPlayerTeam(player);
 
         Color color = Color.web(controller.getTeamColour(team));
         gcDraw.setDrawColor(gc, color);
 
-        Vector size = player.getDimensions();
-
         gc.fillOval(position.getX(), position.getY(), size.getX(), size.getY());
 
-        gcDraw.setDrawColor(gc, Color.WHITE);
-        gcDraw.drawText(gc, position.getX(), position.getY(), 15, player.getName());
+        //gcDraw.setDrawColor(gc, Color.WHITE);
+        //gcDraw.drawText(gc, position.getX(), position.getY(), 15, player.getName());
 
         gcDraw.setDrawColor(gc, Color.BLACK);
     }
 
     private void drawPlayerHasBall(GameObject player, Vector position) {
-        drawPlayer(player, position);
+        drawPlayer(player);
         gcDraw.setDrawColor(gc, Color.YELLOW);
 
         Vector size = player.getDimensions();
@@ -139,9 +167,9 @@ public class StrategyImageExporter {
         for (GameObject gameObject : controller.getGameObjects()) {
             if (gameObject instanceof Player) {
                 tracePlayerMovement(gameObject);
-                drawPlayer(gameObject, controller.getPosition(gameObject));
+                drawPlayer(gameObject);
                 setAtFirstFrame();
-                drawPlayer(gameObject, controller.getPosition(gameObject));
+                drawPlayer(gameObject);
             }
         }
 
